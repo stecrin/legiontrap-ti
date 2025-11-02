ded1c8f7cc2dda6070f0bbe5b569f7f10935eea4
stecrin <stefancringusi@gmail.com>
2025-11-02 13:33:06 +0000

    <0001fb82> CI: finalize formatting for release.config.js

--- release.config.js
+++ release.config.js
@@ -2,22 +2,39 @@ export default {
     branches: ["main"],
     repositoryUrl: "https://github.com/stecrin/legiontrap-ti",
     plugins: [
-      "@semantic-release/commit-analyzer",
-      "@semantic-release/release-notes-generator",
+      [
+        "@semantic-release/commit-analyzer",
+        {
+          preset: "conventionalcommits",
+        },
+      ],
+      [
+        "@semantic-release/release-notes-generator",
+        {
+          preset: "conventionalcommits",
+        },
+      ],
       [
         "@semantic-release/changelog",
         {
           changelogFile: "CHANGELOG.md",
-          changelogTitle: "# Changelog\n\nAll notable changes to this project will be documented in this file.\n",
+          changelogTitle:
+            "# Changelog\n\nAll notable changes to this project will be documented in this file.\n",
         },
       ],
       [
         "@semantic-release/git",
         {
           assets: ["CHANGELOG.md"],
-          message: "🔖 Release ${nextRelease.version}\n\n${nextRelease.notes}",
+          message: "🔖 chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
         },
       ],
-      "@semantic-release/github",
+      [
+        "@semantic-release/github",
+        {
+          assets: [{ path: "CHANGELOG.md", label: "Changelog" }],
+        },
+      ],
     ],
   };
+  
